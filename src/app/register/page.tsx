8f22bd14967fafc5e6411ccd057604a156e56470
Alessandro Viana <alessandroviana830@gmail.com>
2026-03-10 21:57:28 -0300

    Adicionar página de cadastro e login

---
+++ src/app/register/page.tsx
@@ -0,0 +1,26 @@
+import styles from '@/app/styles/formPage.module.css'
+export default function login() {
+return (
+        <main className={ styles.main }>
+            <div className={ styles.textBox }>
+                <p className={ styles.logo }><span className={ styles.pinkText }>Cup&</span>Cake</p>
+                <p className={ styles.description }>O <span className={ styles.pinkText }>CUPCAKE</span> DE MELHOR QUALIDADE ATÉ VOCÊ</p>
+            </div>
+            <div className={ styles.container }>
+                <a href='/login' className={ styles.link }>Login</a>
+                <div className={ styles.formBox }>
+                    <h1 className={ styles.formTitle }>CADASTRO ADMIN</h1>
+                    <form action="/" method="POST" className={ styles.form }>
+                        <label className={ styles.label } htmlFor="email">Email:</label>
+                        <input className={ styles.input } type="text" name="email" id="email" />
+                        <label className={ styles.label } htmlFor="password">Senha:</label>
+                        <input className={ styles.input } type="password" name="password" id="password" />
+                        <label className={ styles.label } htmlFor="password">Confirmar Senha:</label>
+                        <input className={ styles.input } type="password" name="password" id="password" />
+                        <input className= { styles.button } type="submit" value="Logar" />
+                    </form>
+                </div>
+            </div>
+        </main>
+    )
+}
